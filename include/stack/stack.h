d95d0ae077223f9ec704a25280798cf071ca1825
dgunther2001 <danielgunther2001@gmail.com>
2024-04-25 10:57:38 -0400

    Push, pop and print added to the stack, and testing disabled

---
+++ include/stack/stack.h
@@ -0,0 +1,127 @@
+#include "../external_libs/Linked_List/include/linked_list/linked_list.h"
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+template <typename Object>
+class Stack {
+private:
+    Linked_List<Object> stackList;
+    int size;
+
+public:
+    Stack() : stackList() {
+        size = 0;
+    }
+
+    int getSize() {
+        return size;
+    }
+
+    void push(Object obj) {
+        stackList.addNode(obj);
+        size++;
+    }
+
+    void push(Object* obj) {
+        stackList.addNode(obj);
+        size++;
+    }
+
+
+    template <typename T = Object>
+    typename std::enable_if<!std::is_pointer<T>::value, T>::type pop() {
+        static_assert(!std::is_pointer<Object>::value, "Non-pointer type expected");
+        if (size == 0) {
+            throw std::out_of_range("Stack is empty");
+        }
+        Object* returnNode = stackList.getObjAtIndex(size - 1);
+        stackList.deleteNode(returnNode);
+        size--;
+
+        return *returnNode;
+    }
+
+
+    template <typename T = Object>
+    typename std::enable_if<std::is_pointer<T>::value, T>::type pop() {
+        static_assert(std::is_pointer<Object>::value, "Pointer type expected");
+        if (size == 0) {
+            throw std::out_of_range("Stack is empty");
+        }
+        Object* returnNode = stackList.getObjAtIndex(size - 1);
+        stackList.deleteNode(*returnNode);
+        size--;
+
+        return *returnNode;
+    }
+
+    
+    template <typename T = Object, typename std::enable_if<!std::is_same<T, int>::value && 
+    !std::is_same<T, char>::value && 
+    !std::is_same<T, float>::value && 
+    !std::is_same<T, double>::value && 
+    !std::is_same<T, bool>::value && 
+    !std::is_same<T, short>::value && 
+    !std::is_same<T, long>::value && 
+    !std::is_same<T, long long>::value && 
+    !std::is_same<T, unsigned short>::value && 
+    !std::is_same<T, unsigned int>::value && 
+    !std::is_same<T, unsigned long>::value && 
+    !std::is_same<T, unsigned long long>::value>::type* = nullptr>
+    void printStack() {
+        for(int i = size - 1; i >= 0; i--) {
+            if (i == size - 1) {
+                std::cout << "Stack Top:";
+                std::cout << "\t" << *(stackList.getObjAtIndex(i)) << "\n";
+            }
+            else if (i == 0) {
+                std::cout << "Stack Base:";
+                std::cout << "\t"  << *(stackList.getObjAtIndex(i)) << "\n";
+            }
+            else {
+                std::cout << "\t\t"  << *(stackList.getObjAtIndex(i)) << "\n";
+            }
+            
+
+        }
+
+        //std::cout << "----------" << "\n";
+    }
+
+    template <typename T = Object, typename std::enable_if<std::is_same<T, int>::value || 
+    std::is_same<T, char>::value || 
+    std::is_same<T, float>::value || 
+    std::is_same<T, double>::value || 
+    std::is_same<T, bool>::value || 
+    std::is_same<T, short>::value || 
+    std::is_same<T, long>::value || 
+    std::is_same<T, long long>::value || 
+    std::is_same<T, unsigned short>::value || 
+    std::is_same<T, unsigned int>::value || 
+    std::is_same<T, unsigned long>::value || 
+    std::is_same<T, unsigned long long>::value>::type* = nullptr>
+    void printStack() {
+        for(int i = size - 1; i >= 0; i--) {
+            if (i == size - 1) {
+                std::cout << "Stack Top:";
+                std::cout << "\t"  << stackList.getObjAtIndex(i) << "\n";
+            }
+            else if (i == 0) {
+                std::cout << "Stack Base:";
+                std::cout << "\t"  << stackList.getObjAtIndex(i) << "\n";
+            }
+            else {
+                std::cout << "\t\t"  << stackList.getObjAtIndex(i) << "\n";
+            }
+            
+
+        }
+
+        //std::cout << "----------" << "\n";
+    }
+    
+
+
+
+};
--- tests/test.cpp
+++ tests/test.cpp
@@ -1,6 +1,82 @@
+#include "stack/stack.h"
+#include <iostream>
+#include <string>
+
+class myObj {
+private:
+    int ID;
+    std::string name;
+
+public:
+    myObj(int i, std::string n) {
+        ID = i;
+        name = n;
+    }
+
+    int getId() {
+        return ID;
+    }
+
+    std::string getName() {
+        return name;
+    }
+
+    bool operator==(const myObj& other) const {
+        return (name == other.name && ID == other.ID);
+    }
+    
+    
+
+    bool operator!=(const myObj& other) const {
+        return (name != other.name || ID != other.ID);
+    }
+
+    
+    friend std::ostream& operator<< (std::ostream& os, const myObj& s)
+    {
+        os << s.ID << " - " << s.name;
+        return os;
+    }
+
+};
 
 
 int main() {
+    
+    Stack<int> myStack0;
+    myStack0.push(10);
+    myStack0.push(27);
+    myStack0.push(33);
+    myStack0.push(77);
+    myStack0.push(65);
+
+    myStack0.printStack();
+
+    std::cout << "\n";
+    
+
+    Stack<myObj> myStack1;
+
+    myObj* obj1 = new myObj(1, "Joe");
+    myObj* obj2 = new myObj(2, "John");
+
+
+    myStack1.push(obj1);
+    myStack1.push(obj2);
+
+    myStack1.printStack();
+
+    std::cout << "\n";
+
+    myObj obj = myStack1.pop();
+
+    myStack1.printStack();
+
+    std::cout << "\n" << obj << "\n";
+
+
+
+
 
     return 0;
 }
